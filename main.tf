271dfc5597549af9bdcdfae7292984eb8450680b
n920094 <nakabayashi.m@teldevice.co.jp>
2022-10-28 11:22:08 +0900

    first commit

--- main.tf
+++ main.tf
@@ -1,2 +1,21 @@
 terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "TED_EVAL"
+    workspaces {
+      name = "tfc-demo"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+}
+
+resource "aws_instance" "hello-tf-instance" {
+  ami           = var.ami
+  count         = var.hello_tf_instance_count
+  instance_type = var.hello_tf_instance_type
 }
