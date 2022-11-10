560e12ee48a1a2d4dbfff2a73f45ccf3271bb7f9
n920094 <nakabayashi.m@teldevice.co.jp>
2022-11-10 18:38:31 +0900

    fitst commit

--- main.tf
+++ main.tf
@@ -12,8 +12,5 @@ resource "aws_instance" "hello-tf-instance" {
   ami           = var.ami
   count         = var.hello_tf_instance_count
   instance_type = var.hello_tf_instance_type
-  tags = {
-    dep = "develope"
-    sec = "front-end"
-  }
+  #insert tags here
 }
